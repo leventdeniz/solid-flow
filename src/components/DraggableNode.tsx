ce9a69bd9b81cd5d9151839f6291e89498e01c36
Levent Deniz <levent.deniz@check24.de>
2023-02-04 14:39:06 +0100

    reactive nodes

--- src/components/DraggableNode.tsx
+++ src/components/DraggableNode.tsx
@@ -1,73 +1,61 @@
 import { children, Component, createSignal, JSX, JSXElement } from 'solid-js';
-import { gridSize, mainContainerRef } from '~/components/FlowBoard';
+import { gridSize, mainContainerRef, SetPos } from '~/components/FlowBoard';
 
 interface NodeProps {
     identifier: string,
     x: number,
     y: number,
     children: JSX.Element,
-    isDraggable: boolean
+    isDraggable: boolean;
+    setPos: SetPos;
 }
 
-export const DraggableNode: Component<NodeProps> = ({ identifier, x, y, children: _children, isDraggable }) => {
-    const childrenResolver = children(() => _children)
-    const [pos, setPos] = createSignal({x, y});
+export const DraggableNode: Component<NodeProps> = (props) => {
+    const childrenResolver = children(() => props.children)
+    const [isMouseDown, setIsMouseDown] = createSignal(false);
     const width = 80;
     const height = 40;
-    let componentRef: HTMLDivElement | undefined;
 
 
     const onMouseDown = (event: MouseEvent) => {
         event.preventDefault();
+        setIsMouseDown(true);
         document.addEventListener('mousemove', onMouseMove);
         document.addEventListener('mouseup', onMouseUp);
     }
     const onMouseUp = (event: MouseEvent) => {
         event.preventDefault();
+        setIsMouseDown(false);
         document.removeEventListener('mousemove', onMouseMove);
-        if (!componentRef) return
-        componentRef.style.outline = `none`;
-        componentRef.style.cursor = `grab`;
     }
 
     function onMouseMove(event: MouseEvent) {
         event.preventDefault();
-        if (!componentRef) return
-        //debugSnapPoints(draggedElement)
-
-        //diffToLastMouseEventCoords.x = lastRecordedMousePosition.x - event.clientX;
-        //console.log(diffToLastMouseEventCoords.x);
-
-        //if (event.clientX % 40 <= 7 || event.clientX % 40 >= 32) {
-
-        //if (event.clientX % 40 === 0) {
-        //const newX = eleClientRect.x - diffToLastMouseEventCoords.x;
         const newSnapX = (Math.round((event.clientX - (mainContainerRef?.offsetLeft ?? 0)) / gridSize()) * gridSize()) - width / 2;
         const newSnapY = (Math.round((event.clientY - (mainContainerRef?.offsetTop ?? 0)) / gridSize()) * gridSize()) - height / 2;
-        //lastRecordedMousePosition.x = event.clientX;
-        setPos({ x: newSnapX, y: newSnapY })
-        //componentRef.style.transform = `translate(${newSnapX}px, ${newSnapY}px)`;
-        componentRef.innerText = `${newSnapX}`;
-        //}
-        componentRef.style.cursor = `grabbing`;
-        componentRef.style.outline = `2px solid #1940B9`;
+        if (newSnapY !== props.y || newSnapX !== props.x) {
+            props.setPos((v) => ({ x: newSnapX, y: newSnapY }));
+        }
     }
 
     return (
         <div
-            ref={componentRef!}
             onmousedown={onMouseDown}
             onmouseup={onMouseUp}
-            data-id={identifier}
+            data-id={props.identifier}
             style={{
-                transform: `translate(${pos().x}px, ${pos().y}px)`,
+                transform: `translate(${props.x}px, ${props.y}px)`,
                 width: `${width}px`,
                 height: `${height}px`,
                 "line-height": `${height - 2}px`,
+                cursor: isMouseDown() ? 'grabbing' : '',
+                outline: isMouseDown() ? '2px solid #1940B9' : ''
             }}
             class={`card bg-base-100 shadow-l text-center border absolute cursor-grab select-none pointer-events-auto rounded-md`}
         >
             {childrenResolver()}
+            <br/>
+            {props.x}
         </div>
     );
 };
--- src/components/FlowBoard.tsx
+++ src/components/FlowBoard.tsx
@@ -1,38 +1,72 @@
-import { createSignal, For } from 'solid-js';
+import { children, createEffect, createSignal, For, JSX, mapArray } from 'solid-js';
 import { DraggableNode } from '~/components/DraggableNode';
 import { GridIndicator } from '~/components/GridIndicator';
 import { MouseDebug } from '~/components/MouseDebug';
+import { Setter } from 'solid-js/types/reactive/signal';
+import { unwrap } from 'solid-js/store';
 
-const nodes = [
+export interface NodeObj {
+    identifier: string,
+    x: number,
+    y: number,
+    children: JSX.Element,
+    isDraggable: boolean,
+}
+
+const rawArray: NodeObj[] = [
     { identifier: 'test1', x: 40 * 6, y: 40 * 2, children: '1', isDraggable: true },
     { identifier: 'test2', x: 40 * 2, y: 40 * 4, children: '2', isDraggable: true },
     { identifier: 'test3', x: 40 * 6, y: 40 * 4, children: '3', isDraggable: true },
     { identifier: 'test4', x: 40 * 10, y: 40 * 4, children: '4', isDraggable: true },
 ];
 
+export type SetPos = Setter<{ x: number, y: number }>
+type ReturnType = Omit<NodeObj, 'y' | 'x'> & { setPos: SetPos, readonly x: number, readonly y: number };
+
+const nodes = mapArray<NodeObj, ReturnType>(() => rawArray, ({ x, y, ...model }) => {
+    const [pos, setPos] = createSignal({ x, y });
+
+    return {
+        ...model,
+        get x() {
+            return pos().x;
+        },
+        get y() {
+            return pos().y;
+        },
+        setPos,
+    };
+});
 export const [gridSize, setGridSize] = createSignal(20);
 
 export let mainContainerRef: undefined | HTMLDivElement;
 
 export default function FlowBoard() {
     return (
-        <div ref={mainContainerRef} class="w-100vw h-[70vh] overflow-hidden relative bg-base-200" >
-            <div class="w-full h-full top-0 left-0 absolute z-[1] origin-top-left">
-                <svg
-                    id="svg" width="916" height="948"
-                    class="w-full h-full top-0 left-0 absolute origin-top-left overflow-visible"
-                >
-                    <defs></defs>
-                    <g></g>
-                </svg>
-                <div id="nodes">
-                    <For each={nodes} fallback={<div>Loading...</div>}>
-                        {({ children, ...props }) => <DraggableNode  {...props} >{children}</DraggableNode>}
-                    </For>
+        <>
+            <div ref={mainContainerRef} class="w-100vw h-[70vh] overflow-hidden relative bg-base-200">
+
+                <div class="w-full h-full top-0 left-0 absolute z-[1] origin-top-left">
+                    <svg
+                        id="svg" width="916" height="948"
+                        class="w-full h-full top-0 left-0 absolute origin-top-left overflow-visible"
+                    >
+                        <defs></defs>
+                        <g></g>
+                    </svg>
+                    <div id="nodes">
+                        <For each={nodes()} fallback={<div>Loading...</div>}>
+                            {(node) => (
+                                <DraggableNode identifier={node.identifier} x={node.x} y={node.y} isDraggable={node.isDraggable} setPos={node.setPos}>
+                                    {node.children}
+                                </DraggableNode>
+                            )}
+                        </For>
+                    </div>
                 </div>
+                <GridIndicator/>
+                <MouseDebug/>
             </div>
-            <GridIndicator/>
-            <MouseDebug />
-        </div>
+        </>
     );
 }
